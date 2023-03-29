42929dd5f5a623267035521fce1f2daf03a0a527
Almaz Kubakaev <almazkubakaev2607@gmail.com>
2023-03-29 20:01:32 +0600

    creating a class component ClassCounter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
 
   return (
     <div className="App">
-      <Counter />
+      <ClassCounter />
     </div>
   );
 }
